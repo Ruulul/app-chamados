770a691e2034a0e20516d2ac44d0449e62060802
Ruulul <vamjunior01@gmail.com>
2021-11-18 07:35:18 -0300

    Servidor funcionando

--- client2/react/src/Pages/Home.jsx
+++ client2/react/src/Pages/Home.jsx
@@ -14,9 +14,9 @@ const Home = ({user}) => {
   },[])
   return (
     <Grid container direction={{xs: "column", md: "row"}} width="100%">
-      <Grid item xs={12} md={10} minHeight={{xs:1/2, md: 1}} >
+      <Grid item xs={12} md={10} lg={6} minHeight={{xs:1/2, md: 1}} >
         <Card elevation={3} sx={{ padding: 0, height:"100%", width: 1}} >
-            <Grid item container xs={12} md={10} lg={4} sx={{padding: {xs: 1, md: 3}, minHeight: 1/2}}>
+            <Grid item container xs={12} md={10} lg={6} sx={{padding: {xs: 1, md: 3}, minHeight: 1/2}}>
               <Grid item xs={10} md={12} >
                 <Card width={1} height={1/3} sx={{display: "flex",padding: 2, alignItems: "stretch"}}>
                   <Stack spacing={2} pb={2}>
